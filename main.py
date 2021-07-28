023e32ffa22b34fe6f7836797ec59d3be49b732a
ilkerozturk <ilkerozturketu@gmail.com>
2021-07-29 01:56:13 +0300

    Initial commit - Created a hangman game as an exercise and used more complex code parts :)

---
+++ main.py
@@ -0,0 +1,51 @@
+import random
+import string
+
+from words import words
+
+
+def get_a_valid_word(words):
+    word = random.choice(words)  # randomly chooses something from list
+    while '-' in word or ' ' in word:
+        word = random.choice(words)
+
+    return word.upper()
+
+
+def hangman():
+    word = get_a_valid_word(words)
+    word_letters = set(word)  # letters in the word
+    alphabet = set(string.ascii_uppercase)
+    used_letters = set()  # what the user guessed
+    lives = 6
+    # getting the user input
+    while len(word_letters) > 0 and lives > 0:
+
+        # letters used
+        # ' '.join(['a','b','cd']) --> 'a b cd'
+        print('You have ', lives, 'lives left and you have used these letters: ', ' '.join(used_letters))
+
+        # what current word is (ie W-RD)
+
+        word_list = [letter if letter in used_letters else '-' for letter in word]
+        print('Current word is : ', ' '.join(word_list))
+
+        user_letter = input('Guess a letter: '.upper())
+        if user_letter in alphabet - used_letters:
+            used_letters.add(user_letter)
+            if user_letter in word_letters:
+                word_letters.remove(user_letter)
+            else:
+                lives = lives - 1
+                print("Letter is not in the word!")
+        elif user_letter in used_letters:
+            print("You have already used that character. Please try again! ")
+        else:
+            print("Invalid character. Please try again!")
+    if lives > 0:
+        print("You successfully guessed the word : ", word)
+    else:
+        print("You failed. The correct word was : ", word)
+
+
+hangman()
